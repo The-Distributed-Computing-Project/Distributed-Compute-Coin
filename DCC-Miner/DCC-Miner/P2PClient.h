f5b6cc19001ba0a7a99011739531ec2876c09207
sam-astro <77079540+sam-astro@users.noreply.github.com>
2022-05-05 07:42:21 -0400

    Update P2PClient.h

--- DCC-Miner/DCC-Miner/P2PClient.h
+++ DCC-Miner/DCC-Miner/P2PClient.h
@@ -2,12 +2,18 @@
 #define p2pclient_h
 
 #include <string>
-//using namespace std;
-//
-//std::string NormalizedIPString(SOCKADDR_IN addr);
-//
-//void TaskRec();
 
-int StartP2P(std::string addr, std::string port);
+class P2P
+{
+public:
+  //using namespace std;
+  //
+  //std::string NormalizedIPString(SOCKADDR_IN addr);
+  //
+  //void TaskRec();
+  bool CONNECTED_TO_PEER = false;
+
+  int StartP2P(std::string addr, std::string port);
+};
 
 #endif

4fe23222ba29bd473c116fc6961da64809ad493c
sam-astro <77079540+sam-astro@users.noreply.github.com>
2022-12-22 09:58:24 -0500

    Continue adding P2P handling and more comments

--- DCC-Miner/DCC-Miner/P2PClient.cpp
+++ DCC-Miner/DCC-Miner/P2PClient.cpp
@@ -73,12 +73,12 @@ void P2P::TaskRec()
 		/*if (messageAttempt == 4)
 			return;*/
 
-		console.NetworkPrint();
-		console.WriteLine("Checking for requests...");
+		//console.NetworkPrint();
+		//console.WriteLine("Checking for requests...");
 		int t = select(-1, &stReadFDS, 0, 0, &stTimeOut);
 		if (t == SOCKET_ERROR) {
 			console.NetworkErrorPrint();
-			console.WriteLine("Error: Socket Error.");
+			console.WriteLine("Error: Socket Error, trying again...");
 			break;
 		}
 		else {
@@ -89,12 +89,17 @@ void P2P::TaskRec()
 				std::string textVal = std::string(buffer, buffer + iResult);
 				if (textVal == "peer connect") {
 					console.WriteLine("Received initial connection, awaiting confirmation...", console.greenFGColor, "");
-					messageStatus = 1;
+					messageStatus = 1; // Awaiting confirmation status
 				}
 				else if (textVal == "peer success") {
-					console.WriteLine("Dual Connection Confirmation", console.greenFGColor, "");
-					messageStatus = 2;
+					console.WriteLine("Dual Confirmation", console.greenFGColor, "");
+					messageStatus = 2; // Confirmed message status, continue sending our own 
+					                   // confirmation for 4 times, then switch to idle state -1
 					CONNECTED_TO_PEER = true;
+				}
+				else if (textVal == "peer idle") {
+					console.WriteLine("idle...", console.yellowFGColor, "");
+					//messageStatus = -1;
 				}
 				// If peer is requesting data
 				else if (SplitString(textVal, " ")[0] == "request") {
@@ -119,8 +124,8 @@ void P2P::TaskRec()
 					// If peer is giving a block's data
 					else if (SplitString(textVal, " ")[1] == "block") {
 						messageStatus = 1;
-						std::string blockData = SplitString(textVal, " ")[3];
 						int num = std::stoi(SplitString(textVal, " ")[2]);
+						std::string blockData = SplitString(textVal, " ")[3];
 
 						// Save block data to file
 						try
@@ -185,6 +190,7 @@ int P2P::StartP2P(std::string addr, std::string port)
 	console.NetworkPrint();
 	console.WriteLine("Starting P2P Client");
 
+	// Start winsock
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		return 0;
@@ -210,6 +216,7 @@ int P2P::StartP2P(std::string addr, std::string port)
 	//		address.sin_addr.s_addr = INADDR_ANY;
 	//		address.sin_port = htons(port);
 
+	// Bind the socket
 	if (bind(localSocket, (LPSOCKADDR)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR)
 	{
 		console.ErrorPrint();
@@ -246,6 +253,8 @@ int P2P::StartP2P(std::string addr, std::string port)
 	console.WriteLine("HTTP returned: " + httpOut);
 
 	std::string last_tried_ip_port = "";
+	
+	// Start requesting peers to connect to from the handling server
 	for (int t = 0; t < 20; t++)
 		//while (true)
 	{
@@ -259,11 +268,12 @@ int P2P::StartP2P(std::string addr, std::string port)
 		};
 		httpOut = TrimString(http.StartHttpWebRequest("http://api.achillium.us.to/dcc/p2pconn.php?", args));
 
+		// If the request fails or no peers are found, try again after 3 seconds
 		if (httpOut == "" || httpOut.find("waiting") != std::string::npos || httpOut.find(addr + ":" + port) != std::string::npos)
 		{
 			console.NetworkPrint();
-			console.WriteLine("Failed, waiting 3 sec to look for a new peer...");
-			Sleep(3000);
+			console.WriteLine("Failed, waiting 3 sec to ask for a peer...");
+			Sleep(3000); // Wait 3 seconds until next request
 			continue;
 		}
 
@@ -271,6 +281,7 @@ int P2P::StartP2P(std::string addr, std::string port)
 		console.WriteLine("HTTP2 returned: " + httpOut);
 
 		otherIpPort = httpOut;
+		last_tried_ip_port = otherIpPort;
 
 
 		std::string host = SplitString(otherIpPort, ":")[0];
@@ -285,64 +296,123 @@ int P2P::StartP2P(std::string addr, std::string port)
 		// Listen to replies
 		const unsigned int update_interval = 500; // update after every 500 milliseconds
 		std::thread t1(&P2P::TaskRec, this);
+		
+		bool noinput = false;
 
 		// Begin sending messages, and stop when a reply is received
-		for (messageAttempt = 0; messageAttempt < 15; messageAttempt++)
+		for (messageAttempt = 0; messageAttempt < 8; messageAttempt++)
 			//while (true)
 		{
-			std::string msg = messageStatus >= 1 ? "peer success" : "peer connect";
-			char* msgConvert = (char*)msg.c_str();
-			//int err = SafeSend(localSocket, msgConvert, msg.length());
-			//std::cout << err << std::endl;
-
-			console.NetworkPrint();
-			console.WriteLine("Attempt: " + std::to_string(messageAttempt) + "  |  Sending: " + msg);
-
-			// If doing peer confirmation
-			if (messageStatus == 2) {
-				sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
-				Sleep(3000);
-
-				console.WriteLine("Successful connection!", console.greenFGColor, "");
-
-				if (messageAttempt == 4) {
-					console.WriteLine("stopping thread...", console.yellowFGColor, "");
-					stop_thread_1 = true;
-					Sleep(8000);
-					t1.join();
-					stop_thread_1 = false;
-					console.WriteLine("stopped", console.yellowFGColor, "");
-
-					closesocket(localSocket);
-					WSACleanup();
-
-					console.WriteLine("Closing connection...", console.greenFGColor, "");
-
-					return 0;
+			// If not in idle state, continue sending messages
+			if(messageStatus != -1){
+				std::string msg = "";
+				//int err = SafeSend(localSocket, msgConvert, msg.length());
+				//std::cout << err << std::endl;
+	
+				console.NetworkPrint();
+				console.WriteLine("Send attempt: " + std::to_string(messageAttempt));
+	
+				// If doing initial connect request
+				if (messageStatus == 0){
+					msg = "peer connect";
+					char* msgConvert = (char*)msg.c_str();
+					sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
 				}
-			}
-			// Else if replying to height request
-			else if (messageStatus == 3) {
-				msg = "answer height " + std::to_string(blockchainLength);
-				sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
-				Sleep(3000);
-			}
-			// Else if replying to block data request
-			else if (messageStatus == 4) {
-				// Open and read requested block
-				std::ifstream td("./wwwdata/blockchain/block" + std::to_string(reqDat) + ".dccblock");
-				std::stringstream bufferd;
-				bufferd << td.rdbuf();
-				std::string blockText = bufferd.str();
+				// If doing peer confirmation
+				else if (messageStatus == 2 || messageStatus == 1) {
+					msg = "peer success";
+					char* msgConvert = (char*)msg.c_str();
+					sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
+	
+					console.WriteLine("Confirmed", console.greenFGColor, "");
+	
+					if (messageAttempt == 4) {
+						// After multiple confirmations have been sent, switch back to idle mode
+						messageStatus = -1;
+						noinput = false;
+						continue;
+						//console.WriteLine("stopping thread...", console.yellowFGColor, "");
+						//stop_thread_1 = true;
+						//Sleep(8000);
+						//t1.join();
+						//stop_thread_1 = false;
+						//console.WriteLine("stopped", console.yellowFGColor, "");
 
-				msg = "answer block " + std::to_string(reqDat) + TrimString(blockText);
-				sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
+						//closesocket(localSocket);
+						//WSACleanup();
+
+						//console.WriteLine("Closing connection...", console.greenFGColor, "");
+
+						//return 0;
+					}
+				}
+				// Else if replying to height request
+				else if (messageStatus == 3) {
+					msg = "answer height " + std::to_string(blockchainLength);
+					char* msgConvert = (char*)msg.c_str();
+					sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
+				}
+				// Else if replying to block data request
+				else if (messageStatus == 4) {
+					// Open and read requested block
+					std::ifstream td("./wwwdata/blockchain/block" + std::to_string(reqDat) + ".dccblock");
+					std::stringstream bufferd;
+					bufferd << td.rdbuf();
+					std::string blockText = bufferd.str();
+
+					msg = "answer block " + std::to_string(reqDat) + TrimString(blockText);
+					char* msgConvert = (char*)msg.c_str();
+					sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
+				}
+				// Else if requesting chain height
+				else if (messageStatus == 5) {
+					msg = "request height";
+					char* msgConvert = (char*)msg.c_str();
+					sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
+				}
+				// Else if requesting block data
+				else if (messageStatus == 6) {
+					msg = "request block " + std::to_string(reqDat);
+					char* msgConvert = (char*)msg.c_str();
+					sendto(localSocket, msg.c_str(), msg.length(), 0, (sockaddr*)&otherAddr, otherSize);
+				}
+				// Wait 3 seconds before sending next message
 				Sleep(3000);
+			}
+			// Else if in idle state, request input (temporary) and execute that input
+			else if(!noinput) {
+				// Request console input
+				std::string inputCmd = "";
+				std::cout << "Network Input*>  ";
+				std::getline(std::cin, inputCmd);
+				
+				// Reset attempts to 0, since we are currently not sending messages
+				messageAttempt = 0;
+				
+				// If the inputted command is blank or too small to be a command, try again.
+				if(inputCmd.length() <= 2)
+					continue;
+				
+				std::string[] cmdArgs = SplitString(inputCmd, " ");
+				
+				// If user inputted `hight` command
+				if(cmdArgs[0] == "height")
+					messageStatus = 5;
+				// If user inputted `syncblock` command, and an argument
+				else if(cmdArgs[0] == "syncblock" && (sizeof(cmdArgs) / sizeof(cmdArgs[0])) >= 2) {
+					reqDat = std::stoi(cmdArgs[1]); // Block number is the first argument
+					messageStatus = 6;
+				}
+				// If user inputted `noinput` command, stop requesting console input so the main thread is free to reply
+				else if(cmdArgs[0] == "noinput") {
+					noinput = true;
+				}
 			}
 		}
 
 		console.WriteLine("Unsuccessful connection, trying other peer", console.redFGColor, "");
 
+		// Stop the current listening thread and continue
 		stop_thread_1 = true;
 		t1.join();
 		stop_thread_1 = false;

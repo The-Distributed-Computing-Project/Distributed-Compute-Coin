1e2759bc8d1a2f110390a62f4262afb89e2323ae
sam-astro <77079540+sam-astro@users.noreply.github.com>
2024-01-04 21:27:41 -0500

    change port

--- DCC-Miner/DCC-Miner/SettingsConsts.h
+++ DCC-Miner/DCC-Miner/SettingsConsts.h
@@ -1,9 +1,16 @@
 #ifndef SETTINGSCONSTS_H
 #define SETTINGSCONSTS_H
+#pragma once
+
+
+const std::string VERSION = "v0.5.1-alpha";
+const std::string BLOCK_VERSION = "v0.7.0-alpha-coin";
+
+const std::string serverURL = "http://api.achillium.us.to:3030";
+
 
 namespace constants
 {
-    constexpr double pi{ 3.14159 };
     constexpr bool debugPrint{ false };
 
 }

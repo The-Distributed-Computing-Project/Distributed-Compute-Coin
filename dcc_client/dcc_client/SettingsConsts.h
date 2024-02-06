f3e0b39255776bfd91a5d9eec8c64f7b9228e963
sam-astro <77079540+sam-astro@users.noreply.github.com>
2024-02-06 08:07:38 -0500

    Update version, add `DELUGE_VERSION`

--- dcc_client/dcc_client/SettingsConsts.h
+++ dcc_client/dcc_client/SettingsConsts.h
@@ -3,8 +3,9 @@
 
 #define DELUGE_CHUNK_SIZE 32768
 
-const std::string VERSION = "v0.6.0-alpha";
+const std::string VERSION = "v0.7.1-alpha";
 const std::string BLOCK_VERSION = "v0.8.2-alpha-coin";
+const std::string DELUGE_VERSION = "v0.0.1-alpha-coin";
 
 const std::string serverURL = "http://dccpool.us.to";
 

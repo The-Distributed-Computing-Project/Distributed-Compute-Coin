b234adcd3365bb08a86d189bc8f24f76352fb342
sam-astro <77079540+sam-astro@users.noreply.github.com>
2024-02-07 09:08:07 -0500

    Update SettingsConsts.h

--- dcc_client/dcc_client/SettingsConsts.h
+++ dcc_client/dcc_client/SettingsConsts.h
@@ -2,6 +2,8 @@
 #define settings_h
 
 #define DELUGE_CHUNK_SIZE 32768
+#define DELUGE_MAX_SIZE_B 134217728 /* 2^27, also known as 128 megabytes */
+#define DELUGE_MAX_CHUNKS DELUGE_MAX_SIZE_B / DELUGE_CHUNK_SIZE  /* The number of chunks allowed */
 
 const std::string VERSION = "v0.7.2-alpha";
 const std::string BLOCK_VERSION = "v0.8.2-alpha-coin";

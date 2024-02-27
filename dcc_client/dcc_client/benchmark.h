8bee2a4ebb759be50211a4c3187f065a0a245bea
sam-astro <77079540+sam-astro@users.noreply.github.com>
2024-02-27 11:41:57 -0500

    Create benchmark.h

---
+++ dcc_client/dcc_client/benchmark.h
@@ -0,0 +1,15 @@
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include<vector>
+#include <string>
+
+using namespace std;
+using namespace std::chrono;
+
+double benchmark();
+
+#endif
